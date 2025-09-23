d22ac94caadda4a63a93225e2b75b765a79f8e5d
ermi9s <ermiayele1122334477@gmail.com>
2025-09-23 11:04:48 +0300

    fixed bugs

--- resource/reputation/models.py
+++ resource/reputation/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Reputation(models.Model):
-    business = models.ForeignKey('business.Business', on_delete=models.CASCADE, related_name='business-reputation')
+    business = models.ForeignKey('business.Business', on_delete=models.CASCADE, related_name='business_reputation')
     share_count = models.BigIntegerField()
     average_ratting = models.FloatField()
     review_count = models.BigIntegerField()
