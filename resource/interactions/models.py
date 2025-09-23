8c93362fe7045678e9d502ae180a091d82d9483e
ermi9s <ermiayele1122334477@gmail.com>
2025-09-23 09:28:31 +0300

    interactions CRUD

--- resource/interactions/models.py
+++ resource/interactions/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Share(models.Model):
+    ad = models.ForeignKey('ads.Ad', on_delete=models.CASCADE, related_name='ad-share')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='user-share')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Review(models.Model):
+    ad = models.ForeignKey('ads.Ad', on_delete=models.CASCADE, related_name='ad_review')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='reviewer')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"user {self.user} reviewed {self.ad}"
+
+
+class ServiceRatting(models.Model):
+    ad = models.ForeignKey('ads.Ad', on_delete=models.CASCADE, related_name='ad_review')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='reviewer')
+    ratting = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"user {self.user} rated {self.ad}"
+
+
--- resource/reputation/models.py
+++ resource/reputation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Reputation(models.Model):
+    business = models.ForeignKey('business.Business', on_delete=models.CASCADE, related_name='business-reputation')
+    share_count = models.BigIntegerField()
+    average_ratting = models.FloatField()
+    review_count = models.BigIntegerField()
+    overall_score = models.BigIntegerField(default=50)
+    
+
+
