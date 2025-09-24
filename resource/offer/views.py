b30c7103f36b4c31f6d15c9f2e3cfb62a2fa5fb3
danielababu <daniel0ababu@gmail.com>
2025-09-24 09:03:25 +0300

    updated offer

--- resource/offer/views.py
+++ resource/offer/views.py
@@ -1,12 +1,20 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .models import Offer
+from .pagination import OfferPagination
 from .serializers import OfferSerializer
+from rest_framework import filters
+
 
 # Create your views here.
 
 class ListOffer(ListAPIView):
-    queryset = Offer.objects.all()
+    queryset = Offer.objects.filter(status='active') 
+    pagination_class = OfferPagination
     serializer_class = OfferSerializer
+    
+    ordering_fields = ['created_at']
+    search_fields = [ 'description']
+
 
 # POST
 class CreateOffer(CreateAPIView):
